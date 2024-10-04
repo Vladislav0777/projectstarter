56f00b23c0576449099a4ba9508c8fda5e07451e
Зайцев Владислав Алексеевич <zaytsev@cinimex.ru>
2024-10-04 15:55:59 +0300

    update config and compose

--- src/main/java/ru/project/starter/config/SecurityConfig.java
+++ src/main/java/ru/project/starter/config/SecurityConfig.java
@@ -2,16 +2,33 @@ package ru.project.starter.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.authority.mapping.GrantedAuthoritiesMapper;
+import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.client.oidc.web.logout.OidcClientInitiatedLogoutSuccessHandler;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.core.oidc.OidcUserInfo;
+import org.springframework.security.oauth2.core.oidc.user.OidcUserAuthority;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
+import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
+
+import java.net.URI;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @EnableWebSecurity
 @Configuration
@@ -39,7 +56,7 @@ public class SecurityConfig {
             "/upload/"};
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http, OidcClientInitiatedLogoutSuccessHandler oidcLogoutSuccessHandler) throws Exception {
         http
                 .authorizeHttpRequests((authorizationManagerRequestMatcherRegistry) ->
                         authorizationManagerRequestMatcherRegistry
@@ -47,31 +64,22 @@ public class SecurityConfig {
                         .anyRequest()
                         .authenticated()
                 )
-                .csrf(Customizer.withDefaults())
-                .formLogin(httpSecurityFormLoginConfigurer -> {
-                    httpSecurityFormLoginConfigurer.defaultSuccessUrl("/", true);
-                })
-                .logout(httpSecurityLogoutConfigurer -> {
-                    httpSecurityLogoutConfigurer
-                            .logoutUrl("/logout")
-                            .logoutSuccessUrl("/login?logout")
-                            .deleteCookies("JSESSIONID")
-                            .invalidateHttpSession(true)
-                            .clearAuthentication(true)
-                            .permitAll();
-                });
+                .csrf(withDefaults())
+                .oauth2Login(withDefaults())
+                .logout(logout ->
+                        logout.logoutSuccessHandler(oidcLogoutSuccessHandler));
         return http.build();
     }
 
     @Bean
     public UserDetailsService userDetailsService(){
         UserDetails user = User.builder()
-                .username("user")
+                .username("user1")
                 .password(passwordEncoder().encode("123"))
                 .roles("USER")
                 .build();
         UserDetails admin = User.builder()
-                .username("admin")
+                .username("admin1")
                 .password(passwordEncoder().encode("123"))
                 .roles("ADMIN")
                 .build();
@@ -81,5 +89,39 @@ public class SecurityConfig {
     @Bean
     public static PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    @SuppressWarnings("unchecked")
+    public GrantedAuthoritiesMapper userAuthoritiesMapper() {
+
+        return (authorities) -> {
+            Set<GrantedAuthority> mappedAuthorities = new HashSet<>();
+            authorities.forEach(authority -> {
+                if (authority instanceof OidcUserAuthority oidcUserAuthority) {
+                    OidcUserInfo userInfo = oidcUserAuthority.getUserInfo();
+                    Map<String, Object> realmAccess = userInfo.getClaim("realm_access");
+                    Collection<String> realmRoles;
+                    if (realmAccess != null
+                            && (realmRoles = (Collection<String>) realmAccess.get("roles")) != null) {
+                        realmRoles
+                                .forEach(role -> mappedAuthorities.add(new SimpleGrantedAuthority("ROLE_" + role)));
+                    }
+                }
+            });
+            return mappedAuthorities;
+        };
+    }
+
+    @Bean
+    OidcClientInitiatedLogoutSuccessHandler oidcLogoutSuccessHandler(ClientRegistrationRepository clientRegistrationRepository) {
+        OidcClientInitiatedLogoutSuccessHandler successHandler = new OidcClientInitiatedLogoutSuccessHandler(clientRegistrationRepository);
+        successHandler.setPostLogoutRedirectUri(URI.create("http://localhost:8080/").toString());
+        return successHandler;
+    }
+
+    @Bean
+    protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
+        return new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl());
     }
 }
